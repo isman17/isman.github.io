5e8adf8b82e1237e23c7a57f554b44cb847ed9bd
isman17 <39393350+isman17@users.noreply.github.com>
2018-06-07 09:41:30 +0700

    Add files via upload
    portofilioman

---
+++ jskita.js
@@ -0,0 +1,10 @@
+var ubar = document.getElementById('ubar');
+var sbara = document.getElementsByClassName('sbara');
+function bar() {
+  ubar.style.display = "block";
+  sbara.style.display = "block";
+}
+function xbar() {
+  ubar.style.display = "none";
+  sbara.style.display = "none";
+}
